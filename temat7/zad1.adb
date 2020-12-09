159eadca3a40a6103e04004243920f22ae655134
pvrtykv <jotpe2307@gmail.com>
2020-12-09 15:24:15 +0100

    implemented bounded buffer

--- temat7/zad1.adb
+++ temat7/zad1.adb
@@ -1,7 +1,79 @@
-generic 
-    Rozmiar: Natural;
-    type Typ_Elementu is (<>);
-package Buf_Gen is
-    type TBuf is array(Integer range 1..Rozmiar) of Typ_Elementu;
-end Buf_Gen;
+with Ada.Text_IO;
+use Ada.Text_IO;
 
+procedure Zad1 is
+    N : Integer := 10;
+    type BuffArr is Array (1..N) of Integer;
+    protected Buffer is
+        Entry Write(X : in Integer);
+        Entry Read(X : Out Integer);
+    private
+        Buffers : BuffArr;
+        Next_in, Next_out : Integer range 1..N := 1;
+        current : Integer range 0..N := 0;
+    end Buffer;
+
+    protected body Buffer is 
+        Entry Write(X : in Integer) when current < N is
+        begin
+            Buffers(Next_in) := X;
+            Next_in := (Next_in mod N) + 1;
+            current := current + 1;
+        end Write;
+        Entry Read(X : out Integer) when current > 0 is
+        begin
+            X := Buffers(Next_out);
+            Next_out := (Next_out mod N) + 1;
+            current := current - 1;
+        end Read;
+    end Buffer;
+
+    task type Producer(Id : Positive) is
+        Entry Stop;
+    end Producer;
+
+    task body Producer is
+        item: Integer := 0;
+    begin
+        loop
+            select
+                accept Stop;
+                exit;
+            or
+                delay 0.001;
+            end select;
+            item := item + 1;
+            Put_line("Producer" & Integer'Image(Id)
+            & " writing" & Integer'Image(item));
+            Buffer.Write(item);
+        end loop;
+    end Producer;
+
+    task type Consumer(Id : Positive) is
+        Entry Stop;
+    end Consumer;
+
+    task body Consumer is
+        item : Integer;
+    begin
+        loop
+            select
+                accept stop;
+                exit;
+            or
+                delay 0.001;
+            end select;
+            Buffer.Read(item);
+            Put_Line("Consumer" & Integer'Image(Id)
+            & " reads" & Integer'Image(item));
+        end loop;
+    end Consumer;
+
+    P1 : Producer(1);
+    C1 : Consumer(1);
+
+begin  
+    delay 0.2;
+    P1.Stop;
+    C1.Stop;
+end Zad1;
